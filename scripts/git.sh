2b7a98dc779cf671daa4bd75819624f98f21dc8e
tx4x <baumgart.guenter@gmail.com>
2018-01-10 15:09:51 +0100

    =save only

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,2 +1,4 @@
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=999999'
+git config --global user.email "baumgart.guenter@gmail.com"
+git config --global user.name "tx4x"
